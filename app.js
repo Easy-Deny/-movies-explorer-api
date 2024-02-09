c1b110a57fe0367dd3a9b8bcde0b139f8708c42f
Denis Knor <knorred@mail.ru>
2024-02-09 22:00:45 +0300

    feat add app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./errors/error-handler');
+
+const NotFoundError = require('./errors/not-found-error');
+
+mongoose.connect('mongodb://127.0.0.1:27017/moviedb', {
+  useNewUrlParser: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(() => {
+    console.log('Not connected to MongoDB');
+  });
+const app = express();
+const PORT = 3500;
+const userRouter = require('./routes/users');
+const movieRouter = require('./routes/movies');
+
+const allowedCors = [
+  //'https://praktikum.tk',
+  //'http://praktikum.tk',
+  'http://localhost:3000',
+  //'http://easy-deny.pr15.nomoredomainsmonster.ru',
+  //'https://easy-deny.pr15.nomoredomainsmonster.ru',
+];
+
+app.use(function (req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
+app.use(express.json());
+app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+app.use(userRouter);
+app.use(movieRouter);
+app.all('*', (req, res, next) => {
+  next(new NotFoundError('404! Page not found'));
+});
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`port ${PORT}`);
+});
