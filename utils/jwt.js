9f98ee42a88ed711d23f401c7c522605ba9ba8cc
Denis Knor <knorred@mail.ru>
2024-02-15 15:29:47 +0300

    fix jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET = 'SECRET_KEY' } = process.env;
+//const { JWT_SECRET = 'SECRET_KEY' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const getJwtToken = (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
+const getJwtToken = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'SECRET_KEY', { expiresIn: '7d' });
 
 module.exports = {
   getJwtToken,
