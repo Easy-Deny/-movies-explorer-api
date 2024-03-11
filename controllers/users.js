c800c4121fe636ca41509f7bbc65ed240e4f17cc
Denis Knor <knorred@mail.ru>
2024-03-11 16:08:04 +0300

    fix req data login user

--- controllers/users.js
+++ controllers/users.js
@@ -46,7 +46,7 @@ const login = (req, res, next) => {
             return Promise.reject(new UnauthorizedError('Password is not correct'));
           }
           const token = getJwtToken({ _id: user._id });
-          return res.status(200).send({ token, user });
+          return res.status(200).send({ token, email: user.email, name: user.name });
         });
     })
     .catch(next);
